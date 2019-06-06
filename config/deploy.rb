90fbcce2a20b373be20d34506812d88c05c1e964
nagisa-miura <nagisaue@icloud.com>
2019-06-06 14:54:59 +0900

    capistranoバージョン追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.11.0'
 
 set :application, 'chat-space-1'
 set :repo_url,  'git@github.com:nagisa-miura/chat-space-1.git'
